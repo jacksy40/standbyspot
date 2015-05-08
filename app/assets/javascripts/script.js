7588511d8c0e418a7651305a498ee80ddce71972
Jack <jack.sy@live.com>
2015-05-08 18:24:27 -0400

    heroku

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,7 +1,8 @@
 $(function() {
   $('#new_inquiry').on('ajax:complete', function(e, data, status, xhr){
     $('#inquiry_message').val('Thank you, your inquiry was sent.');
-
+  })
   $('#new_conversation').on('ajax:complete', function(e, data, status, xhr){
     $('#conversation_message').val('message sent.');
+  })
 });
